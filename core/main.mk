e1c4d78a94d156096a062b2b68a07ff11457a60f
napodan <david.guenet@gmail.com>
2013-07-19 20:29:19 +0200

    Update to use jhead 4.2.2_r1.2 and move AndConf

--- core/main.mk
+++ core/main.mk
@@ -173,6 +173,19 @@ $(info ***************************************************************)
 $(error stopping)
 endif
 
+# -----------------------------------------------------------------
+# Variable to check java support level inside PDK build.
+# Not necessary if the components is not in PDK.
+# not defined : not supported
+# "sdk" : sdk API only
+# "platform" : platform API supproted
+TARGET_BUILD_JAVA_SUPPORT_LEVEL := platform
+
+# -----------------------------------------------------------------
+# The pdk (Platform Development Kit) build
+include build/core/pdk_config.mk
+
+# -----------------------------------------------------------------
 ###
 ### In this section we set up the things that are different
 ### between the build variants
