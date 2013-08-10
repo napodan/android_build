10504ddde0183e6847f4475ca69cb20f341639c2
napodan <david.guenet@gmail.com>
2013-08-10 09:28:45 +0200

    Update some files to 4.3_r2.1

--- core/product_config.mk
+++ core/product_config.mk
@@ -162,6 +162,13 @@ ifdef unbundled_goals
 $(unbundled_goals): $(MAKECMDGOALS)
 endif # unbundled_goals
 
+# Default to building dalvikvm on hosts that support it...
+ifeq ($(HOST_OS),linux)
+# ... or if the if the option is already set
+ifeq ($(WITH_HOST_DALVIK),)
+  WITH_HOST_DALVIK := true
+endif
+endif
 
 # ---------------------------------------------------------------
 # Include the product definitions.
