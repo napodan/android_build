aab3ca45bf884ff116fdd71114437640d0a00b55
napodan <david.guenet@gmail.com>
2013-08-12 21:37:03 +0200

    Update some files to 4.3_r2.1

--- target/product/full_base.mk
+++ target/product/full_base.mk
@@ -23,7 +23,7 @@ $(call inherit-product, frameworks/base/data/sounds/AudioPackage5.mk)
 # Get the TTS language packs
 $(call inherit-product-if-exists, external/svox/pico/lang/all_pico_languages.mk)
 
-# Get the list of languages.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+# Get a list of languages.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/locales_full.mk)
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/generic.mk)
