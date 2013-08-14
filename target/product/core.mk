583e8a8164aa66e707aa486876635def157e7b33
napodan <david.guenet@gmail.com>
2013-08-14 22:16:43 +0200

    Remove zoneinfo includes
    these includes are now in bionic/libc

--- target/product/core.mk
+++ target/product/core.mk
@@ -71,9 +71,6 @@ PRODUCT_PACKAGES := \
     UserDictionaryProvider \
     PackageInstaller \
     DefaultContainerService \
-    zoneinfo.dat \
-    zoneinfo.idx \
-    zoneinfo.version
 
 
 # host-only dependencies
